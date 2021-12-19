2126a4100f324b09bf30bb5b77b7845fb6f80fe5
Max Walther <maxiwalther@mac.com>
2021-12-19 14:03:21 +0100

    verbosity configurable

--- internal/app/cmd/root.go
+++ internal/app/cmd/root.go
@@ -5,14 +5,17 @@ import (
 	"os"
 
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 var (
 	rootCmd = &cobra.Command{
-		Use:   "aoc",
-		Short: "All in one AdventOfCode binary",
+		Use:              "aoc",
+		Short:            "All in one AdventOfCode binary",
+		TraverseChildren: true,
 	}
+	verbose int = 0
 )
 
 func Execute() {
@@ -24,9 +27,23 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	rootCmd.Flags().CountVarP(&verbose, "verbose", "v", "verbose output")
 }
 
 func initConfig() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	switch verbose {
+	case 0:
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case 1:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case 2:
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case 3:
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	}
+	log.WithLevel(zerolog.GlobalLevel()).Msgf("logging level set to %v", zerolog.GlobalLevel())
+	
 }
