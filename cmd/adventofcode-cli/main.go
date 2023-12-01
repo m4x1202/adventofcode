59978f0822729ebda0fc6ebf74ed1849ab16e130
Max Walther <maxiwalther@mac.com>
2023-12-01 14:41:56 +0100

    Fix remaining preparation for 2023

--- cmd/adventofcode-cli/main.go
+++ cmd/adventofcode-cli/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/m4x1202/adventofcode/internal/app/cmd"
 	_ "github.com/m4x1202/adventofcode/internal/app/cmd/2021"
 	_ "github.com/m4x1202/adventofcode/internal/app/cmd/2022"
+	_ "github.com/m4x1202/adventofcode/internal/app/cmd/2023"
 )
 
 func main() {
