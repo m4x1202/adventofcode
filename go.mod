1d57de7155059e9b9513a74e22263625b4265d2e
Max Walther <maxiwalther@mac.com>
2023-12-01 08:32:05 +0100

    Prep 2023 advent of code

--- go.mod
+++ go.mod
@@ -4,20 +4,20 @@ go 1.19
 
 require (
 	github.com/beefsack/go-astar v0.0.0-20200827232313-4ecf9e304482
-	github.com/rs/zerolog v1.28.0
-	github.com/spf13/cast v1.5.0
-	github.com/spf13/cobra v1.6.1
+	github.com/rs/zerolog v1.31.0
+	github.com/spf13/cast v1.6.0
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9
+	golang.org/x/exp v0.0.0-20231127185646-65229373498e
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/app/2023/day00/main.go
@@ -0,0 +1,77 @@
+// day00 is the template package which can be copied, modified to its final location
+package day00
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/m4x1202/adventofcode/resources"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cast"
+)
+
+const (
+	DAY = "00"
+)
+
+var (
+	dayLogger = log.With().
+			Str("day", DAY).
+			Logger()
+	partLogger zerolog.Logger
+)
+
+func ExecutePart(p uint8) {
+	preparedInput := prepareInput(readPuzzleInput())
+	switch p {
+	case 1:
+		part1Func(preparedInput)
+	case 2:
+		part2Func(preparedInput)
+	default:
+		panic("part does not exist")
+	}
+}
+
+func part1Func(preparedInput any) uint64 {
+	partLogger = dayLogger.With().
+		Int("part", 1).
+		Logger()
+	partLogger.Info().Msg("Start")
+	var puzzleAnswer uint64
+
+	// Logic here
+	puzzleAnswer = cast.ToUint64(0)
+	return puzzleAnswer
+}
+
+func part2Func(preparedInput any) uint64 {
+	partLogger = dayLogger.With().
+		Int("part", 2).
+		Logger()
+	partLogger.Info().Msg("Start")
+	var puzzleAnswer uint64
+
+	// Logic here
+	puzzleAnswer = cast.ToUint64(0)
+	return puzzleAnswer
+}
+
+func readPuzzleInput() string {
+	content, err := resources.InputFS.ReadFile(fmt.Sprintf("2022/day%s/input.txt", DAY))
+	if err != nil {
+		dayLogger.Fatal().Err(err).Send()
+	}
+	return string(content)
+}
+
+func prepareInput(rawInput string) any {
+	input := strings.Split(strings.TrimSuffix(rawInput, "\n"), "\n")
+	dayLogger.Info().Msgf("length of input file: %d", len(input))
+	dayLogger.Debug().Msgf("plain input: %v", input)
+
+	// Required input conversion here
+
+	return input
+}
